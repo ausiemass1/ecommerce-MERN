870be69e04d751118598624642e3f0225facae8c
ausiemass1 <ausiemass@gmail.com>
2026-01-01 09:45:15 +1300

    feat(home): add home page with sidebar

---
+++ frontend/src/components/sideBar.tsx
@@ -0,0 +1,27 @@
+const Sidebar = () => {
+    return (
+      <div className="collection">
+        <a href="#!" className="collection-item active">
+          Food
+        </a>
+  
+        <a href="#!" className="collection-item">All Day Menu</a>
+        <a href="#!" className="collection-item">Breakfast</a>
+        <a href="#!" className="collection-item">Burgers</a>
+        <a href="#!" className="collection-item">Beef</a>
+        <a href="#!" className="collection-item">Chicken & Fish</a>
+        <a href="#!" className="collection-item">Salads & Wraps</a>
+        <a href="#!" className="collection-item">Sides</a>
+        <a href="#!" className="collection-item">Happy Meal</a>
+  
+        <div className="divider"></div>
+  
+        <a href="#!" className="collection-item">Bundles & Offers</a>
+        <a href="#!" className="collection-item">Drinks</a>
+        <a href="#!" className="collection-item">Desserts</a>
+      </div>
+    );
+  };
+  
+  export default Sidebar;
+  
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,41 @@
+import ProductCard from "../components/ProductCard";
+import Sidebar from "../components/sideBar";
+import { useEffect, useState } from "react";
+import { type Product } from "../types/Product";
+import axios from "axios"
+
+
 function Home() {
-    return <h1>Home Page</h1>;
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Product[]>("http://localhost:4000/api/products")
+      .then(res => setProducts(res.data));
+  }, []);
+  return (
+    <div className="container">
+      <div className="row">
+        
+        {/* Sidebar */}
+        <div className="col s12 m3">
+          <Sidebar />
+        </div>
+
+        {/* Products */}
+        <div className="col s12 m9">
+          <div className="row">
+            {products.map(product => (
+              <ProductCard
+                product={product}
+              />
+            ))}
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
   }
   
   export default Home;
