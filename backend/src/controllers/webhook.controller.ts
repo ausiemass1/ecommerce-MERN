8963e78ba781434025df5c08686f2dc2a0fcd97f
ausiemass1 <ausiemass@gmail.com>
2026-01-08 14:16:41 +1300

    fix: fixed webhook controller to save product unit price

--- backend/src/controllers/webhook.controller.ts
+++ backend/src/controllers/webhook.controller.ts
@@ -64,12 +64,23 @@ export const createWebhook = async (req: Request, res: Response) => {
           customerEmail: session.customer_details?.email,
           customerName: session.customer_details?.name,
           shipping: session.shipping_details ?? null,
+          // items: lineItems.data.map((item) => ({
+          //   name: item.description,
+          //   quantity: item.quantity,
+          //   amount_total: item.amount_total,
+          //   priceId: item.price?.id,
+          // })),
+
+
           items: lineItems.data.map((item) => ({
             name: item.description,
-            quantity: item.quantity,
-            amount_total: item.amount_total,
-            priceId: item.price?.id,
+            quantity: item.quantity ?? 1,
+            unit_price: item.price?.unit_amount ?? 0,
+            total_price: item.amount_total ?? 0,
+            currency: item.currency,
           })),
+          
+          
           currency: session.currency,
           amount_total: session.amount_total,
           payment_status: session.payment_status,
