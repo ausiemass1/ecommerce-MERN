5a0e2ecb58ee0c844c3e7ae77390cb42eaff4cea
ausiemass1 <ausiemass@gmail.com>
2026-01-08 19:57:26 +1300

    Refactor(getAll Orders): Refactor get all orders to use pagenation

--- backend/src/controllers/admin/admin.orders.controller.ts
+++ backend/src/controllers/admin/admin.orders.controller.ts
@@ -1,25 +1,26 @@
 import { Request, Response } from "express";
 import Order from "../../models/ordersModel";
+import { paginate } from "../../utils/paginate";
 
 // view all orders
-export const getAllOrders = async (req: Request, res: Response) => {
-  const page = Number(req.query.page) || 1;
-  const limit = 10;
+// export const getAllOrders = async (req: Request, res: Response) => {
+//   const page = Number(req.query.page) || 1;
+//   const limit = 10;
 
-  const orders = await Order.find()
-    .sort({ createdAt: -1 })
-    .skip((page - 1) * limit)
-    .limit(limit);
+//   const orders = await Order.find()
+//     .sort({ createdAt: -1 })
+//     .skip((page - 1) * limit)
+//     .limit(limit);
 
-  const total = await Order.countDocuments();
+//   const total = await Order.countDocuments();
 
-  res.json({
-    orders,
-    total,
-    page,
-    pages: Math.ceil(total / limit),
-  });
-};
+//   res.json({
+//     orders,
+//     total,
+//     page,
+//     pages: Math.ceil(total / limit),
+//   });
+// };
 
 //view single order
 export const getOrderById = async (req: Request, res: Response) => {
@@ -34,4 +35,20 @@ export const getOrderById = async (req: Request, res: Response) => {
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch order" });
   }
+};
+
+
+
+//view all orders
+export const getAllOrders = async (req: Request, res: Response) => {
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const result = await paginate(Order, {
+    page,
+    limit,
+    sort: { createdAt: -1 },
+  });
+
+  res.json(result);
 };
