72b6b0b472683ef323074f7667687a104020a52e
ausiemass1 <ausiemass@gmail.com>
2026-01-15 22:13:51 +1300

    fix: add ErrorBoundary component to handle runtime errors in React app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 import App from "./App.tsx";
 import { UserAuthProvider } from "./context/UserAuthContext.tsx";
 // import React from "react";
-import ErrorBoundary from "../../frontend-admin/src/components/ErrorBoundary.tsx";
+import ErrorBoundary from "./components/ErrorBoundary.tsx";
 
 createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
