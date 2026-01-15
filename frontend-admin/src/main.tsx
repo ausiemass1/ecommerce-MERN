dbd07936b7b41221cddc8ecf43228f174bfc3047
ausiemass1 <ausiemass@gmail.com>
2026-01-15 16:33:47 +1300

    fix: update dependencies and fix ErrorBoundary import

--- frontend-admin/src/main.tsx
+++ frontend-admin/src/main.tsx
@@ -3,15 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 import "materialize-css/dist/css/materialize.min.css";
 import "./App.css";
 import App from "./App.tsx";
-import React from "react";
+// import React from "react";
 import ErrorBoundary from "./components/ErrorBoundary.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <ErrorBoundary>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </ErrorBoundary>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
