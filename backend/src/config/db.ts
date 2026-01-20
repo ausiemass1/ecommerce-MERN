226262b495a2f4d5cb901233222f1186b4eaf7f9
ausiemass1 <ausiemass@gmail.com>
2026-01-20 18:27:58 +1300

    Refactor: Update database configuration for authentication login

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL as string);
+    await mongoose.connect(process.env.MONGO_URL as string, {
+      dbName: process.env.MONGO_DB_NAME,
+    });
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error);
